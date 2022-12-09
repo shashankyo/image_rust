8a4850f28ad9a1cd1c563a65144d9ffeb24e3e93
shashank <shashank944533@gmail.com>
2022-12-09 10:37:29 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use error_chain::error_chain;
+use std::io::copy;
+use std::fs::File;
+use tempfile::Builder;
+
+error_chain!{
+    foreign_links{
+        IO(std::io::Error);
+        HttRequest(reqwest::Error);
+
+    }
+}
+
+#[tokio::main]
+fn main() -> Result<()>{
+    let tmp_dir = Builder::new().prefix("example").tempdir()?;
+
+    let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+    let response = reqwest::get(target).await?;
+
+    let mut dest = {
+         let fname = response 
+         .url()
+         .path_segments()
+         .and_then(|segments| segments.last())
+         .and_then(|name| if name.is_empty(){None}else{Some(name)})
+         .unwrap_or("tmp.bin");
+
+        println!("file to download: '{}",fname);
+        let fname = tmp_dir.path().join(fname)
+        println!("eill be located under: '{:?}'",fname);
+        File::create(fname)?
+};
+    let content = response.text().await?;
+    copy(&mut content.as_bytes(), &mut der)
+    Ok(())
+}
